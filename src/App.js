4856a6dac1e74a7af3a35029c127c2e89f626111
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-02-13 18:33:51 +0100

    First slide finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './dist/css/main.css';
+import "./dist/css/main.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className='h1'><span className='html-code'>&lt;h1&gt;</span>Hello, my name is <span className='blue-color-text'>Mykhaylo</span><span className='html-code'>&lt;/h1&gt;</span></h1>
+      {/* Fisrt Page */}
+      <div className="main-container">
+        <div className="title-wrapper">
+          <h1 className="margin-left">
+            <span className="html-code">&lt;h1&gt;</span>Hello, my name is 
+            <span className="blue-color-text"> Mykhaylo</span>
+            <span className="html-code">&lt;/h1&gt;</span>
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
