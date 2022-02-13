0ffd632af836b2f6a3b935b25a0976a14bcd9302
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-02-13 22:38:29 +0100

    Social Side Bar added & social links added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Jumbotron />
-      <SocialSideBar />
+      < SocialSideBar />
     </div>
   );
 }
--- src/SocialSideBar.js
+++ src/SocialSideBar.js
@@ -1,9 +1,40 @@
-import React from "react"
+import React from "react";
+import instagram from "./img/instagram.svg";
+import linkedin from "./img/linkedin.svg";
+import GitHub from "./img/GitHub.svg";
+import telegram from "./img/telegram.svg";
+import gmail from "./img/gmail.svg";
 
-export default function SocialSideBar(){
-    return(
-        <div className="side-bar">
-
-        </div>
-    )
+export default function SocialSideBar() {
+  return (
+    <div className="side-bar">
+      <ul>
+        <li>
+          <a href="https://instagram.com/tymofyeyev?utm_medium=copy_link">
+            <img src={instagram}></img>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/mykhaylo-tymofyeyev">
+            <img src={linkedin}></img>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/sargon17">
+            <img src={GitHub}></img>
+          </a>
+        </li>
+        <li>
+          <a href="https://t.me/Mykhaylo17">
+            <img src={telegram}></img>
+          </a>
+        </li>
+        <li>
+          <a href="mailto:mykhaylo.tymofyeyev@gmail.com">
+            <img src={gmail}></img>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
