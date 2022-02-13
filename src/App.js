44d91c157990eb1b15482b7a52aa421a0f1d23f4
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-02-13 21:30:02 +0100

    social bar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./dist/css/main.css";
 import React from "react"
 import Jumbotron from "./Jumbotron"
+import SocialSideBar from "./SocialSideBar";
 
 function App() {
   return (
     <div className="App">
-      < Jumbotron />
+      <Jumbotron />
+      <SocialSideBar />
     </div>
   );
 }
---
+++ src/SocialSideBar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function SocialSideBar(){
+    return(
+        <div className="side-bar">
+
+        </div>
+    )
+}
