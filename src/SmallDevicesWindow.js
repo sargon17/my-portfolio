0fc669e7cf1d5f8c1a0cef6ea020dbfd27ac3890
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-02-14 10:38:40 +0100

    Animation added + spell errors solved

--- src/SmallDevicesWindow.js
+++ src/SmallDevicesWindow.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function SmallDevicesWindow() {
   return (
-    <div className="main-container">
+    <div className="main-container small-devices-show">
       <h3>
-        I'm sorry but this page is aviable only for <span className="blue-color-text"> bigger devices </span> at the moment, come back later
+        I'm sorry but this page is available only for <span className="blue-color-text"> bigger devices </span> at the moment, come back later
       </h3>
     </div>
   );
