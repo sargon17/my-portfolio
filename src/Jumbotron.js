ef8b993a5d8d882bf3422b4f029b284f78b407a7
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-02-18 13:24:01 +0100

    bem naming method implementing & mobile media queries on work

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Jumbotron() {
   return (
-    <div className="small-devices-hidden">
+    <div className="small-devices-hidden" id="jumbotron">
       <div className="main-container">
-        <div className="title-wrapper margin-left">
-          <h1>
-            <span className="html-code">&lt;h1&gt;</span>Hello, my name is
-            <span className="blue-color-text"> Mykhaylo</span>
-            <span className="html-code">&lt;/h1&gt;</span>
-            <span className="cursor"></span>
+        <div className="jumbotron__wrapper__title margin-left">
+          <h1 className="jumbotron__title">
+            <span className="jumbotron__title__code">&lt;h1&gt;</span>Hello, my name is
+            <span className="blue-color-text jumbotron__title__name"> Mykhaylo</span>
+            <span className="jumbotron__title__code">&lt;/h1&gt;</span>
+            <span className="jumbotron__title__cursor"></span>
           </h1>
         </div>
       </div>
