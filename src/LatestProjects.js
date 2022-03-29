9473c8f4f8ed983959697a61b210796e6b840eef
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-30 00:31:34 +0200

    added new projects

--- src/LatestProjects.js
+++ src/LatestProjects.js
@@ -3,46 +3,55 @@ import Project from "./Project";
 import projects from "./projectsData.js";
 
 function getProjects(projects) {
+  let index = 1;
   let project = projects.map((item) => {
+    let projectId = `latest_project-${index}`;
+    let nextProjectId = `#latest_project-${index + 1}`;
+    index++;
+    console.log(projects);
     return (
-      <Project
-        title={item.title}
-        img={item.img}
-        subtitle={item.subtitle}
-        text={item.text}
-        liveLink={item.liveLink}
-        githubLink={item.githubLink}
-        tech={item.tech}
-      />
+      <div id={projectId}>
+        <div className="container mt__container projects">
+          <div className="row my-2 my-lg-5">
+            <div className="col">
+              <h2>
+                My latest <span className="mt__t-color--blue">projects</span>
+              </h2>
+            </div>
+          </div>
+          <Project
+            title={item.title}
+            img={item.img}
+            subtitle={item.subtitle}
+            text={item.text}
+            liveLink={item.liveLink}
+            githubLink={item.githubLink}
+            tech={item.tech}
+          />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <div
+                className={
+                  index - 1 === projects.length
+                    ? "btn-wrapper btn-wrapper--none"
+                    : "btn-wrapper"
+                }
+              >
+                <a href={nextProjectId}>
+                  <i className="fa-solid fa-chevron-down"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   });
   return project;
 }
 
 export default function LatestProjects() {
-  return (
-    <div id="latest_project-1">
-      <div className="container mt__container projects">
-        <div className="row my-2 my-lg-5">
-          <div className="col">
-            <h2>
-              My latest <span className="mt__t-color--blue">projects</span>
-            </h2>
-          </div>
-        </div>
-        {getProjects(projects)}
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="btn-wrapper btn-wrapper--none">
-              <a href="#section3">
-                <i className="fa-solid fa-chevron-down"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return getProjects(projects);
 }
--- src/projectsData.js
+++ src/projectsData.js
@@ -1,11 +1,68 @@
 import train_img from "../src/img/train-ticket-project.png";
+import font_ratio_img from "../src/img/font-ratio-project.jpg";
+import mine_field_img from "../src/img/mine-field-project.png";
 
 const projects = [
   {
+    key: "font-ratio-project",
+    title: "Font ratio calculator",
+    img: font_ratio_img,
+    subtitle: "Personal project.",
+    text: "Web app made with vanilla JavaScript, which calculates the ratio of the font size of the text in the input field, created for web designer & developers. Fully responsive & designed by Me.",
+    liveLink: "sargon17.github.io/font-ratio/",
+    githubLink: "https://github.com/sargon17/font-ratio",
+    tech: [
+      {
+        name: "Figma",
+        style: "pink",
+      },
+      {
+        name: "HTML5",
+        style: "orange",
+      },
+      {
+        name: "CSS3",
+        style: "blue",
+      },
+      {
+        name: "JavaScript",
+        style: "yellow",
+      },
+    ],
+  },
+  {
+    key: "mine-field-project",
+    title: "Mine field",
+    img: mine_field_img,
+    subtitle: "Boolean Careers homework project.",
+    text: "JavaScript project, which creates a mine field based on the size of the field and the number of mines, the player can chose dificulty and track the relative records. Fully responsive & designed by Me.",
+    liveLink: "https://sargon17.github.io/js-campominato-dom/",
+    githubLink: "https://github.com/sargon17/js-campominato-dom",
+    tech: [
+      {
+        name: "Figma",
+        style: "pink",
+      },
+      {
+        name: "HTML5",
+        style: "orange",
+      },
+      {
+        name: "CSS3",
+        style: "blue",
+      },
+      {
+        name: "JavaScript",
+        style: "yellow",
+      },
+    ],
+  },
+  {
+    key: "train-ticket-project",
     title: "Train ticket generator",
     img: train_img,
     subtitle: "Boolean Careers homework project.",
-    text: "Simple JavaScript project, which generates a train ticket based on the age of the customer and the distance of the trip. Fully responsive & designed by Me.",
+    text: "A JavaScript project, which generates a train ticket based on the age of the customer and the distance of the trip. Fully responsive & designed by Me.",
     liveLink: "https://sargon17.github.io/js-biglietto-treno/",
     githubLink: "https://github.com/sargon17/js-biglietto-treno",
     tech: [
