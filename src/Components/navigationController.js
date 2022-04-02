147c7fae62a9d15cafd7ed05ed5ad6f6e0c4af50
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-04-02 12:28:38 +0200

    working on slider controller

--- src/Components/navigationController.js
+++ src/Components/navigationController.js
@@ -1,4 +1,4 @@
-import react from "react";
+import react, { useState } from "react";
 import left from "../img/chevron-left.svg";
 import right from "../img/chevron-right.svg";
 
@@ -9,21 +9,23 @@ function dotsNumber(number) {
   }
   return dots;
 }
-const [current, setCurrent] = useState(1);
-
-setCurrent(current, value) {
-  return current + value;
-};
 
 export default function NavigationController(props) {
+  let initialCurrent = 1;
+
+  const [current, setCurrent] = useState(initialCurrent);
   //   const [dots, setDots] = useState(dotsNumber(props.items));
 
+  const handleClick = (e) => {
+    setCurrent(dotIndex);
+  };
   return (
     <div className="mt__projects__control">
       <a
         className="mt__control-btn"
         id="btn-left"
-        href={`#project-${index > 1 ? --index : index}`}
+        href={`#project-${current > 1 ? current : current + 1}`}
+        onClick={() => setCurrent((current) => current - 1)}
       >
         <img src={left} alt="left" />
       </a>
@@ -31,7 +33,8 @@ export default function NavigationController(props) {
       <a
         className="mt__control-btn"
         id="btn-right"
-        href={`#project-${index < props.items ? ++index : index}`}
+        href={`#project-${current < 3 ? current : current - 1}`}
+        onClick={handleClick(1)}
       >
         <img src={right} alt="right" />
       </a>
