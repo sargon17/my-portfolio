df4f73228094491502a4ab37207c9e7f3f12a82d
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-09-27 22:14:44 +0200

    typing feature done

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -9,6 +9,39 @@ export default function Jumbotron(props) {
     y: 1000,
   });
 
+  const [text, setText] = useState("");
+  const [name, setName] = useState("");
+
+  const typingAnimation = () => {
+    setText("");
+    setName("");
+    let txt = "Hello, my name is ";
+    let myName = "Mykhaylo";
+
+    let randomSpeed = Math.floor(Math.random() * 200) + 100;
+
+    // await before typing
+
+    // write hello text
+    for (let i = 0; i < txt.length; i++) {
+      // randomSpeed = Math.floor(Math.random() * 200) + 100;
+      setTimeout(function () {
+        setText((text) => text + txt.charAt(i));
+      }, randomSpeed * i);
+    }
+
+    // write name
+    for (let i = 0; i < myName.length; i++) {
+      // randomSpeed = Math.floor(Math.random() * 200) + 100;
+      setTimeout(function () {
+        setName((name) => name + myName.charAt(i));
+      }, randomSpeed * (i + txt.length));
+    }
+  };
+  useEffect(() => {
+    typingAnimation();
+  }, []);
+
   const handleMouseMove = (e) => {
     setCursor({
       x: e.clientX,
@@ -29,17 +62,17 @@ export default function Jumbotron(props) {
           <div className="row">
             <div className="col-12 col-md-10">
               <h1 className="jumbotron__title">
-                <span className="jumbotron__title__code">&lt;h1&gt;</span>Hello,
-                my name is{" "}
+                <span className="jumbotron__title__code">&lt;h1&gt;</span>
+                {text}
                 <span
                   className="mt__t-color--blue jumbotron__title__name"
                   onMouseEnter={() => setIsPhotoVisible(true)}
                   onMouseLeave={() => setIsPhotoVisible(false)}
                 >
-                  Mykhaylo
+                  {name}
                 </span>
-                <span className="jumbotron__title__code">&lt;/h1&gt;</span>
                 <span className="jumbotron__title__cursor"></span>
+                <span className="jumbotron__title__code">&lt;/h1&gt;</span>
               </h1>
             </div>
           </div>
